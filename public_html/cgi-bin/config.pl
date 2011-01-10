85c9a767087241c9135c7825be470da9340f8bd6
Heracles <22sas.heracles@gmail.com>
2011-01-10 17:29:04 +0000

    Ponemos el radio máximo para suministrar AF a 30 Km desde la ciudad suministrada. fixed #5
    git-svn-id: https://xp-dev.com/svn/DangerZone2/trunk@20 b8441048-3770-4138-88a3-4a168d8b665c

--- public_html/cgi-bin/config.pl
+++ public_html/cgi-bin/config.pl
@@ -533,7 +533,7 @@ $big_red="<strong><font size=\"+2\" color=\"ff0000\"><br>";
 
 ## @Heracles@20110107@
 $AF_SUM=3;  # porcentaje de recupero  de AF por cada transporte que aterriza
-$AF_SUM_MAX_RAD=50000; # Radio máximo de distancia de una AF a la ciudad suministrada para poder suministrar AF
+$AF_SUM_MAX_RAD=30000; # Radio máximo de distancia de una AF a la ciudad suministrada para poder suministrar AF
 
 $MIN_BOMBERS_DIST=50000; 
 $MAX_BOMBERS_DIST=70000;
