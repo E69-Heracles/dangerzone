ffbfc313506870b468cc84b6b6850488af836f80
Heracles <22sas.heracles@gmail.com>
2011-06-22 17:21:22 +0000

    Activadas estadísticas en google Analytics
    git-svn-id: https://xp-dev.com/svn/DangerZone2/trunk@85 b8441048-3770-4138-88a3-4a168d8b665c

--- public_html/dz_page_header.php
+++ public_html/dz_page_header.php
@@ -15,14 +15,18 @@
 
 </head>
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12501535-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-12839878-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
 <body>
 	<table  width="100%" cellpadding="0" cellspacing="0" border=0 height="100%">
 	<tr height="30" class="header">
